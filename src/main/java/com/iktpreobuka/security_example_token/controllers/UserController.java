6dca08fe8170965276dfd318d59a0a77d3d3f397
Aleksandar Soskic <soskic@live.com>
2018-05-26 11:51:05 +0200

    dodat txt proba

--- src/main/java/com/iktpreobuka/security_example_token/controllers/UserController.java
+++ src/main/java/com/iktpreobuka/security_example_token/controllers/UserController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -44,5 +45,8 @@ public class UserController {
 	public ResponseEntity<?> listUsers() {
 		return new ResponseEntity<List<UserEntity>>(getDummyDB(), HttpStatus.OK);
 	}
-	@RequestMapping( POST ...
+	@RequestMapping(method = RequestMethod.POST)
+	public UserEntity addNewUser(@RequestBody UserEntity newUser) {
+		return UserEntity.save(newUser);
+	}
 }
